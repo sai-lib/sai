91b291c3678e3c50e0d10e17789e2b24fdc44647
Aaron Allen <hello@aaronmallen.me>
2025-01-18 19:59:19 -0600

    Enhance `Sai`
    This enhances `Sai` with all the decoration methods from
    `Sai::Decorator` as well as adds inclusion functionality to allow Sai to
    be included in a class or module and provide color and styling support
    to that class or module.

--- lib/sai.rb
+++ lib/sai.rb
@@ -15,12 +15,93 @@ require 'singleton'
 # different terminal capabilities. Like its artistic namesake, it combines simplicity and sophistication to bring rich,
 # adaptive color to your terminal interfaces
 #
+# When included in a class or module, Sai provides the following instance methods:
+# * {#decorator} - Returns a new instance of {Decorator} for method chaining
+# * {#terminal_color_support} - Returns the color support capabilities of the current terminal
+#
+# The Sai module itself responds to all the same methods as {Decorator}, excluding methods used for applying
+# decorations (apply, call, decorate, encode). These methods are directly delegated to a new {Decorator} instance
+#
 # @author {https://aaronmallen.me Aaron Allen}
 # @since unreleased
 #
 # @api public
+#
+# @example Using Sai as a module
+#   class MyClass
+#     include Sai
+#   end
+#
+#   my_class = MyClass.new
+#   my_class.decorator.red.on_blue.bold.decorate('Hello, World!')
+#   #=> "\e[38;2;205;0;0m\e[48;2;0;0;238m\e[1mHello, World!\e[0m"
+#
+#   my_class.terminal_color_support.true_color? # => true
+#
+# @example Using Sai directly
+#   Sai.red.on_blue.bold.decorate('Hello, World!')
+#   #=> "\e[38;2;205;0;0m\e[48;2;0;0;238m\e[1mHello, World!\e[0m"
+#
+#   Sai.support.true_color? # => true
 module Sai
   class << self
+    ignored_decorator_methods = %i[apply call decorate encode]
+    Decorator.instance_methods(false).reject { |m| ignored_decorator_methods.include?(m) }.each do |method|
+      define_method(method) do |*arguments, **keyword_arguments|
+        Decorator.new(send(:color_mode)).public_send(method, *arguments, **keyword_arguments)
+      end
+    end
+
+    # @rbs!
+    #   def black: () -> self
+    #   def blink: () -> self
+    #   def blue: () -> self
+    #   def bold: () -> self
+    #   def bright_black: () -> self
+    #   def bright_blue: () -> self
+    #   def bright_cyan: () -> self
+    #   def bright_green: () -> self
+    #   def bright_magenta: () -> self
+    #   def bright_red: () -> self
+    #   def bright_white: () -> self
+    #   def bright_yellow: () -> self
+    #   def conceal: () -> self
+    #   def cyan: () -> self
+    #   def dim: () -> self
+    #   def green: () -> self
+    #   def italic: () -> self
+    #   def magenta: () -> self
+    #   def no_blink: () -> self
+    #   def no_conceal: () -> self
+    #   def no_italic: () -> self
+    #   def no_reverse: () -> self
+    #   def no_strike: () -> self
+    #   def no_underline: () -> self
+    #   def normal_intensity: () -> self
+    #   def on_black: () -> self
+    #   def on_blue: () -> self
+    #   def on_bright_black: () -> self
+    #   def on_bright_blue: () -> self
+    #   def on_bright_cyan: () -> self
+    #   def on_bright_green: () -> self
+    #   def on_bright_magenta: () -> self
+    #   def on_bright_red: () -> self
+    #   def on_bright_white: () -> self
+    #   def on_bright_yellow: () -> self
+    #   def on_cyan: () -> self
+    #   def on_green: () -> self
+    #   def on_magenta: () -> self
+    #   def on_red: () -> self
+    #   def on_white: () -> self
+    #   def on_yellow: () -> self
+    #   def rapid_blink: () -> self
+    #   def red: () -> self
+    #   def reverse: () -> self
+    #   def strike: () -> self
+    #   def underline: () -> self
+    #   def white: () -> self
+    #   def yellow: () -> self
+
     # The supported color modes for the terminal
     #
     # @author {https://aaronmallen.me Aaron Allen}
@@ -55,5 +136,50 @@ module Sai
     def color_mode
       Thread.current[:sai_color_mode] ||= Terminal::Capabilities.detect_color_support
     end
+  end
+
+  # A helper method to initialize an instance of {Decorator}
+  #
+  # @author {https://aaronmallen.me Aaron Allen}
+  # @since unreleased
+  #
+  # @api public
+  #
+  # @example Initialize a new instance of {Decorator}
+  #   class MyClass
+  #     include Sai
+  #   end
+  #
+  #   MyClass.new.decorator.blue.on_red.bold.decorate('Hello, world!')
+  #   #=> "\e[38;5;21m\e[48;5;160m\e[1mHello, world!\e[0m"
+  #
+  # @return [Decorator] the Decorator instance
+  # @rbs () -> Decorator
+  def decorator
+    Decorator.new(Terminal::Capabilities.detect_color_support)
+  end
+
+  # The supported color modes for the terminal
+  #
+  # @author {https://aaronmallen.me Aaron Allen}
+  # @since unreleased
+  #
+  # @api public
+  #
+  # @example Check the color support of the terminal
+  #   class MyClass
+  #     include Sai
+  #   end
+  #
+  #   MyClass.new.terminal_color_support.ansi? # => true
+  #   MyClass.new.terminal_color_support.basic? # => true
+  #   MyClass.new.terminal_color_support.bit8? # => true
+  #   MyClass.new.terminal_color_support.no_color? # => false
+  #   MyClass.new.terminal_color_support.true_color? # => true
+  #
+  # @return [Support] the color support
+  # @rbs () -> Support
+  def terminal_color_support
+    Sai.support
   end
 end
--- spec/sai_spec.rb
+++ spec/sai_spec.rb
@@ -3,6 +3,12 @@
 require 'spec_helper'
 
 RSpec.describe Sai do
+  let(:color_mode) { Sai::Terminal::ColorMode::TRUE_COLOR }
+
+  before do
+    allow(Sai::Terminal::Capabilities).to receive(:detect_color_support).and_return(color_mode)
+  end
+
   describe '.support' do
     subject(:support) { described_class.support }
 
@@ -15,6 +21,179 @@ RSpec.describe Sai do
       second_call = described_class.support
 
       expect(first_call).to be(second_call)
+    end
+  end
+
+  describe 'module inclusion' do
+    let(:including_class) do
+      Class.new do
+        include Sai
+      end
+    end
+
+    let(:instance) { including_class.new }
+
+    describe '#decorator' do
+      subject(:decorator) { instance.decorator }
+
+      let(:decorator_double) { instance_double(Sai::Decorator) }
+
+      before do
+        allow(Sai::Decorator).to receive(:new).with(color_mode).and_return(decorator_double)
+        decorator
+      end
+
+      it 'is expected to return a new Decorator instance' do
+        expect(Sai::Decorator).to have_received(:new).with(color_mode)
+      end
+    end
+
+    describe '#terminal_color_support' do
+      subject(:terminal_color_support) { instance.terminal_color_support }
+
+      it 'is expected to return the support instance' do
+        expect(terminal_color_support).to be(described_class.support)
+      end
+    end
+  end
+
+  describe 'delegated decorator methods' do
+    shared_examples 'a delegated method without arguments' do |method|
+      subject(:delegated_call) { described_class.public_send(method) }
+
+      let(:decorator_instance) { instance_spy(Sai::Decorator) }
+
+      before do
+        allow(Sai::Decorator).to receive(:new).and_return(decorator_instance)
+        delegated_call
+      end
+
+      it 'is expected to create a new Decorator with the current color mode' do
+        expect(Sai::Decorator).to have_received(:new).with(color_mode)
+      end
+
+      it 'is expected to delegate the method call to the Decorator instance' do
+        expect(decorator_instance).to have_received(method).with(no_args)
+      end
+    end
+
+    shared_examples 'a delegated method with hex argument' do
+      subject(:delegated_call) { described_class.hex('#FF0000') }
+
+      let(:decorator_instance) { instance_spy(Sai::Decorator) }
+
+      before do
+        allow(Sai::Decorator).to receive(:new).and_return(decorator_instance)
+        delegated_call
+      end
+
+      it 'is expected to create a new Decorator with the current color mode' do
+        expect(Sai::Decorator).to have_received(:new).with(color_mode)
+      end
+
+      it 'is expected to delegate the method call to the Decorator instance' do
+        expect(decorator_instance).to have_received(:hex).with('#FF0000')
+      end
+    end
+
+    shared_examples 'a delegated method with rgb arguments' do
+      subject(:delegated_call) { described_class.rgb(255, 0, 0) }
+
+      let(:decorator_instance) { instance_spy(Sai::Decorator) }
+
+      before do
+        allow(Sai::Decorator).to receive(:new).and_return(decorator_instance)
+        delegated_call
+      end
+
+      it 'is expected to create a new Decorator with the current color mode' do
+        expect(Sai::Decorator).to have_received(:new).with(color_mode)
+      end
+
+      it 'is expected to delegate the method call to the Decorator instance' do
+        expect(decorator_instance).to have_received(:rgb).with(255, 0, 0)
+      end
+    end
+
+    # Test methods without arguments
+    describe '.red' do
+      include_examples 'a delegated method without arguments', :red
+    end
+
+    describe '.blue' do
+      include_examples 'a delegated method without arguments', :blue
+    end
+
+    describe '.bold' do
+      include_examples 'a delegated method without arguments', :bold
+    end
+
+    describe '.italic' do
+      include_examples 'a delegated method without arguments', :italic
+    end
+
+    # Test methods with arguments
+    describe '.hex' do
+      include_examples 'a delegated method with hex argument'
+    end
+
+    describe '.rgb' do
+      include_examples 'a delegated method with rgb arguments'
+    end
+
+    describe 'excluded methods' do
+      %i[apply call decorate encode].each do |method|
+        it "is not expected to respond to ##{method}" do
+          expect(described_class).not_to respond_to(method)
+        end
+      end
+    end
+  end
+
+  describe 'thread safety' do
+    describe '.color_mode' do
+      subject(:color_mode_value) { described_class.send(:color_mode) }
+
+      before do
+        Thread.current[:sai_color_mode] = nil
+      end
+
+      it 'is expected to store color mode in thread local storage' do
+        expect { color_mode_value }
+          .to change { Thread.current[:sai_color_mode] }
+          .from(nil)
+          .to(color_mode)
+      end
+
+      it 'is expected to memoize the color mode per thread' do # rubocop:disable RSpec/MultipleExpectations
+        first_call = described_class.send(:color_mode)
+        described_class.send(:color_mode)
+
+        expect(Sai::Terminal::Capabilities)
+          .to have_received(:detect_color_support).once
+        expect(first_call).to eq(color_mode)
+      end
+    end
+
+    describe 'thread isolation' do
+      it 'is expected to maintain color mode isolation' do
+        Thread.current[:sai_color_mode] = color_mode
+        alternate_thread = Thread.new do
+          Thread.current[:sai_color_mode] = Sai::Terminal::ColorMode::BASIC
+          Thread.current[:sai_color_mode]
+        end
+
+        expect(alternate_thread.value).to eq(Sai::Terminal::ColorMode::BASIC)
+      end
+
+      it 'is expected to maintain the original thread color mode' do
+        Thread.current[:sai_color_mode] = color_mode
+        Thread.new do
+          Thread.current[:sai_color_mode] = Sai::Terminal::ColorMode::BASIC
+        end.join
+
+        expect(Thread.current[:sai_color_mode]).to eq(color_mode)
+      end
     end
   end
 end
